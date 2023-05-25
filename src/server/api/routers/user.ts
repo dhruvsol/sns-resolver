e4b63ad8462ae12185a11cd09433223b0aeb2d66
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-05-25 09:45:04 +0530

    feat: added schema

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,45 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { User } from '@prisma/client';
+import { z } from 'zod';
+import { createTRPCRouter, publicProcedure } from '~/server/api/trpc';
+import { prisma } from '~/server/db';
+
+export const userRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(
+      z.object({
+        domain: z.string(),
+        publickey: z.string(),
+        signature: z.string(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      const res = (await prisma.user.create({
+        data: {
+          domain: input.domain,
+          publickey: input.publickey,
+          signature: input.signature,
+        },
+      })) as User;
+
+      return res ?? null;
+    }),
+
+  get: publicProcedure
+    .input(
+      z.object({
+        domain: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+      const res = await prisma.user.findUnique({
+        where: {
+          domain: input.domain,
+        },
+      });
+
+      return res;
+    }),
+});
